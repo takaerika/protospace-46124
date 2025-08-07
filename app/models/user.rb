e1368c227ac2900d7039594fb473b947c660223a
takaerika <erika.nemoto12@gmail.com>
2025-08-07 10:12:05 +0900

    profileのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :occupation, presence: true
   validates :position, presence: true
+  validates :profile, presence: true
+  
 
   has_many :prototypes
   has_many :comments
